8f07fc78cd22e3f8260c593f951e5a870b955d80
Weerayuth Aridej <135725986+weerayuth9516@users.noreply.github.com>
2023-08-03 00:31:38 +0700

    feat: create footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Header />
       {/* <SideBar /> */}
       {/* <Content /> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="footer">Footer</div>
-  )
+    <footer>
+      <div className="footer_bg"></div>
+      <div className="contact-container">
+        <div className="footer-title">Get in touch!</div>
+        <div className="contact-detail">
+          weerayuth9516@gmail.com | 093-9959516
+        </div>
+        <div className="social-icon">
+          <img src="src\image\facebook.png" />
+          <img src="src\image\instagram.png" />
+          <img src="src\image\linkedin.png" />
+          <img src="src\image\twitter.png" />
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
